9b925fd0c9117a20577f4a1dd0d8a1ccd29c2bd3
Usama Kamran <ukabthebest@gmail.com>
2018-05-28 20:48:11 +0500

    Added Sortable Table for Leaves Summary

--- src/views/Leaves/LeaveAdminTab.jsx
+++ src/views/Leaves/LeaveAdminTab.jsx
@@ -1,25 +1,103 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import SmartDataTable from 'react-smart-data-table';
 import { fetchAllLeaves } from "../../api/leave";
-import {fetchAllLeavesFailure, fetchAllLeavesSuccess} from "../../actions/leave";
+import { fetchAllLeavesFailure, fetchAllLeavesSuccess } from "../../actions/leave";
+import Table from 'material-ui/Table'
+import { TableCell, TableRow, TableHead, TableBody, TableSortLabel } from 'material-ui/Table'
+import ToolTip from 'material-ui/Tooltip'
+
+
+const tableHead = [
+  { id: 'name', numeric: false, disablePadding: false, label: 'Name' },
+  { id: 'start_date', numeric: false, disablePadding: false, label: 'Start' },
+  { id: 'end_date', numeric: false, disablePadding: false, label: 'End' },
+  { id: 'status', numeric: false, disablePadding: false, label: 'Status' }
+];
 
 class LeaveAdminTab extends React.Component{
-  
+  constructor(props){
+    super(props);
+    this.handleSort = this.handleSort.bind(this);
+    this.state = {
+      orderBy: null,
+      order: null,
+      allLeaves: this.props.allLeaves
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({allLeaves: nextProps.allLeaves})
+  }
+
+  handleSort = (property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    const allLeaves =
+      order === 'desc'
+        ? this.state.allLeaves.sort((a, b) => (b[orderBy] < a[orderBy] ? -1 : 1))
+        : this.state.allLeaves.sort((a, b) => (a[orderBy] < b[orderBy] ? -1 : 1));
+
+    this.setState({ allLeaves, order, orderBy });
+  };
+
   componentDidMount(){
     this.props.fetchAllLeaves();
   }
-  
+
   render(){
     const { allLeaves } = this.props;
+    const { order, orderBy } = this.state;
     return(
-      <SmartDataTable
-        filterValue={"Account Owner"}
-        data={allLeaves}
-        name="Test"
-        className={"table"}
-        sortable
-      />
+      <Table>
+        <TableHead>
+          <TableRow>
+            {
+              tableHead.map((column) => (
+                <TableCell
+                  key={column.id}
+                  numeric={column.numeric}
+                  padding={column.disablePadding ? 'none' : 'default'}
+                  sortDirection={orderBy === column.id ? order : false}
+                >
+                  <ToolTip
+                    title={"Sort"}
+                    placement={column.numeric ? 'bottom-end' : 'bottom-start' }
+                    enterDelay={300}
+                  >
+                    <TableSortLabel
+                      active={orderBy === column.id}
+                      direction={order}
+                      onClick={this.handleSort.bind(this,column.id)}
+                    >
+                      {column.label}
+                    </TableSortLabel>
+                  </ToolTip>
+                </TableCell>
+              ), this)
+            }
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            {
+              allLeaves.map((leave,index) => (
+                <TableRow
+                  hover
+                  key={index}
+                >
+                  <TableCell>{leave.id}</TableCell>
+                  <TableCell>{leave.start_date}</TableCell>
+                  <TableCell>{leave.end_date}</TableCell>
+                  <TableCell>{leave.status}</TableCell>
+                </TableRow>
+              ))
+            }
+        </TableBody>
+      </Table>
     )
   }
 }
