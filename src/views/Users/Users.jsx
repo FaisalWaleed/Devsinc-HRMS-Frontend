ad13fdfe5244854f9f287df395d5d4df8a0b5846
Usama Kamran <ukabthebest@gmail.com>
2018-06-06 21:27:28 +0500

    Refactored User Table for more control

--- src/views/Users/Users.jsx
+++ src/views/Users/Users.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid } from "material-ui";
-import { RegularCard, Button, Table, ItemGrid, Permissible } from "components";
+import { RegularCard, Button, ItemGrid, Permissible, Muted } from "components";
 import { connect } from 'react-redux';
 import {fetchUsers, deleteUser, editUser, createUser, activateUser} from "../../api/user";
 import {
@@ -20,119 +20,37 @@ import { map } from 'lodash';
 import { hasPermission } from "../../helpers/permissionsHelper";
 import Avatar from 'material-ui/Avatar';
 import Tooltip from 'material-ui/Tooltip';
+import Table, { TableRow, TableHead, TableCell, TableBody } from 'material-ui/Table'
+import { withStyles } from 'material-ui/styles';
+import tableStyle from "variables/styles/tableStyle";
 
 class Users extends React.Component{
   constructor(props){
     super(props);
     this.handleCreateUserSubmit = this.handleCreateUserSubmit.bind(this);
     this.handleEditUserSubmit = this.handleEditUserSubmit.bind(this);
+    this.handleUserRowClick = this.handleUserRowClick.bind(this);
   }
-  
+
   componentDidMount() {
     this.props.fetchUsers();
   }
-  
-  userWithButtons = (user) => {
-    const { id, name, email,dob, emergency_contact_person_number, emergency_contact_person_relation, permanent_address, join_date, buddy_id, first_name, last_name, image, title, contact_number, employment_history, reporting_to, manager, deleted_at } = user;
-    const requiredFields = [ <Avatar src={image} />, name, title,email,contact_number, manager ];
-    return [
-      ...requiredFields,
-      deleted_at
-        ?
-        <Permissible
-          requiredPermissions={["users_restore_user"]}
-        >
-          <Tooltip title={`Unblock ${name}`}>
-            <Lock
-              onClick={
-                this.props.openModal.bind(this,
-                  types.DELETE_MODAL,
-                  {
-                    deleteAction: activateUser(id,activateUserSuccess,activateUserFailure),
-                    resourceType: 'user',
-                    title: 'Are you sure you want to Activate this user?',
-                    message: ' '
-                  }
-                )
-              }
-            
-            
-            />
-          </Tooltip>
-        </Permissible>
-        :
-        <Permissible
-          requiredPermissions={["users_destroy"]}
-        >
-          <Tooltip title={`Block ${name}`}>
-          <LockOpen style={{'marginRight': '10px'}}
-                    onClick={
-                      this.props.openModal.bind(this,
-                        types.DELETE_MODAL,
-                        {
-                          deleteAction: deleteUser(
-                            id,
-                            deleteUserSuccess,
-                            deleteUserFailure
-                          ),
-                          resourceType: 'user',
-                          title: 'Are you sure you want to Deactivate this user?',
-                          message: ' '
-                        }
-                      )
-                    }
-          />
-          </Tooltip>
-        </Permissible>,
-      <Permissible
-        requiredPermissions={["users_update_all"]}
-      >
-        <Edit
-          onClick={
-            this.props.openModal.bind(this, types.FORM_MODAL,
-              {
-                form:
-                  <UserForm
-                    initialValues={{
-                      id,
-                      email,
-                      first_name,
-                      last_name,
-                      title,
-                      reporting_to,
-                      buddy_id,
-                      dob,
-                      contact_number,
-                      emergency_contact_person_number,
-                      emergency_contact_person_relation,
-                      permanent_address,
-                      join_date,
-                      employment_history,
-                    }}
-                    onSubmit={this.handleEditUserSubmit}
-                    isNew={false}
-                  />,
-                title: `Edit ${email}`,
-              }
-            )
-          }
-        />
-      </Permissible>
-    ];
-  };
-  
+
+  handleUserRowClick(){
+
+  }
+
   handleCreateUserSubmit(values){
     this.props.clearFormErrors();
     this.props.createUser(values)
   }
-  
+
   handleEditUserSubmit(values){
     this.props.editUser(values);
   };
-  
+
   render(){
-    const users = map(this.props.users, this.userWithButtons);
-    const { userPermissions } = this.props;
+    const { userPermissions, users, classes } = this.props;
     return(
       <Grid container>
         <ItemGrid xs={12} sm={12} md={12}>
@@ -140,27 +58,145 @@ class Users extends React.Component{
             cardTitle="People"
             cardSubtitle="Every one from your organization"
             content={
-              <div>
-                <Permissible
-                  requiredPermissions={["users_create"]}
-                >
-                  <Button onClick={this.props.openModal.bind(this,types.FORM_MODAL,{title: 'Create New User', form: <UserForm onSubmit={this.handleCreateUserSubmit} isNew={true} />, fullscreen: true  })} color="primary">Create a New User</Button>
-                </Permissible>
-                <Table
-                  tableHeaderColor="primary"
-                  tableHead={[
-                    "Image",
-                    "Name",
-                    "Title",
-                    "Email",
-                    "Contact",
-                    "Manager",
-                    `${hasPermission(userPermissions,["users_destroy"],true) ? "Delete" : ''}`,
-                    `${hasPermission(userPermissions,["users_update_all"],true) ? "Edit" : ''}`
-                  ]}
-                  tableData={users}
-                />
-              </div>
+              <Grid container>
+                <ItemGrid xs={12} sm={12} md={12}>
+                  <Permissible
+                    requiredPermissions={["users_create"]}
+                  >
+                    <Button onClick={this.props.openModal.bind(this,types.FORM_MODAL,{title: 'Create New User', form: <UserForm onSubmit={this.handleCreateUserSubmit} isNew={true} />, fullscreen: true  })} color="primary">Create a New User</Button>
+                  </Permissible>
+                </ItemGrid>
+                <ItemGrid xs={12} sm={12} md={12}>
+                  <div className={classes.tableResponsive}>
+                    <Table className={classes.table}>
+                      <TableHead className={classes.primaryTableHeader}>
+                        <TableRow>
+                          <TableCell className={classes.tableCell + " " + classes.tableHeadCell}>Image</TableCell>
+                          <TableCell className={classes.tableCell + " " + classes.tableHeadCell}>Name</TableCell>
+                          <TableCell className={classes.tableCell + " " + classes.tableHeadCell}>Title</TableCell>
+                          <TableCell className={classes.tableCell + " " + classes.tableHeadCell}>Email</TableCell>
+                          <TableCell className={classes.tableCell + " " + classes.tableHeadCell}>Contact</TableCell>
+                          <TableCell className={classes.tableCell + " " + classes.tableHeadCell}>Manager</TableCell>
+                          <TableCell className={classes.tableCell + " " + classes.tableHeadCell}>
+                            {hasPermission(userPermissions,["users_update_all"],true) ? "Edit" : ''}
+                          </TableCell>
+                          <TableCell className={classes.tableCell + " " + classes.tableHeadCell}>
+                            {hasPermission(userPermissions,["users_destroy"],true) ? "Delete" : ''}
+                          </TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        { users.length ?
+                          users.map((user,index) => (
+                            <TableRow key={index}
+                                      onClick={this.handleUserRowClick.bind(this,user)}
+                                      hover
+                            >
+                              <TableCell className={classes.tableCell}>
+                                <Avatar src={user.image} />
+                              </TableCell>
+                              <TableCell className={classes.tableCell}>{user.name}</TableCell>
+                              <TableCell className={classes.tableCell}>{user.title}</TableCell>
+                              <TableCell className={classes.tableCell}>{user.email}</TableCell>
+                              <TableCell className={classes.tableCell}>{user.contact_number}</TableCell>
+                              <TableCell className={classes.tableCell}>{user.manager}</TableCell>
+                              <TableCell className={classes.tableCell}>
+                                <Permissible
+                                  requiredPermissions={["users_update_all"]}
+                                >
+                                  <Edit
+                                    onClick={
+                                      this.props.openModal.bind(this, types.FORM_MODAL,
+                                        {
+                                          form:
+                                            <UserForm
+                                              initialValues={{
+                                                id: user.id,
+                                                email: user.email,
+                                                first_name: user.first_name,
+                                                last_name: user.last_name,
+                                                title: user.title,
+                                                reporting_to: user.reporting_to,
+                                                buddy_id: user.buddy_id,
+                                                dob: user.dob,
+                                                contact_number: user.contact_number,
+                                                emergency_contact_person_number: user.emergency_contact_person_number,
+                                                emergency_contact_person_relation: user.emergency_contact_person_relation,
+                                                permanent_address: user.permanent_address,
+                                                join_date: user.join_date,
+                                                employment_history: user.employment_history,
+                                              }}
+                                              onSubmit={this.handleEditUserSubmit}
+                                              isNew={false}
+                                            />,
+                                          title: `Edit ${user.email}`,
+                                        }
+                                      )
+                                    }
+                                  />
+                                </Permissible>
+                              </TableCell>
+                              <TableCell className={classes.tableCell}>
+                                {user.deleted_at ?
+                                  <Permissible
+                                    requiredPermissions={["users_restore_user"]}
+                                  >
+                                    <Tooltip title={`Unblock ${user.name}`}>
+                                      <Lock
+                                        onClick={
+                                          this.props.openModal.bind(this,
+                                            types.DELETE_MODAL,
+                                            {
+                                              deleteAction: activateUser(user.id, activateUserSuccess, activateUserFailure),
+                                              resourceType: 'user',
+                                              title: 'Are you sure you want to Activate this user?',
+                                              message: ' '
+                                            }
+                                          )
+                                        }
+
+
+                                      />
+                                    </Tooltip>
+                                  </Permissible>
+                                  :
+                                  <Permissible
+                                    requiredPermissions={["users_destroy"]}
+                                  >
+                                    <Tooltip title={`Block ${user.name}`}>
+                                      <LockOpen style={{'marginRight': '10px'}}
+                                                onClick={
+                                                  this.props.openModal.bind(this,
+                                                    types.DELETE_MODAL,
+                                                    {
+                                                      deleteAction: deleteUser(
+                                                        user.id,
+                                                        deleteUserSuccess,
+                                                        deleteUserFailure
+                                                      ),
+                                                      resourceType: 'user',
+                                                      title: 'Are you sure you want to Deactivate this user?',
+                                                      message: ' '
+                                                    }
+                                                  )
+                                                }
+                                      />
+                                    </Tooltip>
+                                  </Permissible>
+                                }
+                              </TableCell>
+                            </TableRow>
+                          ))
+                          :
+                          <TableRow>
+                            <TableCell><Muted style={{display: 'block'}}>Nothing to show</Muted></TableCell>
+                          </TableRow>
+                        }
+                      </TableBody>
+                    </Table>
+                  </div>
+                </ItemGrid>
+              </Grid>
             }
           />
         </ItemGrid>
@@ -187,4 +223,4 @@ function mapStateToProps(state){
   }
 }
 
-export default Users = connect(mapStateToProps,mapDispatchToProps)(Users);
+export default Users = connect(mapStateToProps,mapDispatchToProps)(withStyles(tableStyle)(Users));
