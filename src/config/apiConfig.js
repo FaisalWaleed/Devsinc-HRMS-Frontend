6b32bb1be1b382dac42f644660eb03cf6f153d45
Usman Asif <mail.usmanasif@gmail.com>
2018-07-04 16:41:22 +0500

    changed api config

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 //This file contains all configuration related to Application
 
 //Specify the endpoint of auth for redux-token-auth
-export const AUTH_URL = 'http://api.devsuite.com/api/v1/auth';
-export const BASE_URL = 'http://api.devsuite.com/api/v1/';
+export const AUTH_URL = 'https://devsuiteapi.herokuapp.com/api/v1/auth';
+export const BASE_URL = 'https://devsuiteapi.herokuapp.com/api/v1/';
 export const LEAVES_QUOTA = 14;
